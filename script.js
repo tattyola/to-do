71da47ef0fa67fb2cef65695759e1a6e1023d514
Tatsiana A <astrouskaya@yahoo.com>
2022-08-21 10:27:02 -0400

    completed todo list

--- script.js
+++ script.js
@@ -1,23 +1,74 @@
 console.log('---JS---');
+const list = JSON.parse(localStorage.getItem('list'));
+//     [
+//     {
+//         id: 'id1',
+//         title: 'Learn JS',
+//         done: true
+//     },
+//     {
+//         id: 'id2',
+//         title: 'Learn React',
+//         done: true
+//     },
+//     {
+//         id: 'id3',
+//         title: 'Get job!',
+//         done: false
+//     },
+// ];
+const listElement = document.getElementById('list');
+const todoInput = document.getElementById('todoInput');
 
-const list = [
-    {
-        id: 1,
-        title: 'Learn JS'
-    },
-    {
-        id: 2,
-        title: 'Learn React'
-    },
-    {
-        id: 2,
-        title: 'Get job!'
-    },
-];
+function render() {
+    listElement.innerHTML = null;
+    list.forEach(el => {
+        const listItem = document.createElement('li');
+        listItem.setAttribute('class', el.done? 'done': 'progress')
+        const listItemText = document.createTextNode(el.title);
+        listItem.appendChild(listItemText);
 
-list.forEach(el => {
-    const listItem = document.createElement('li');
-    const listItemText = document.createTextNode(el.title);
-    listItem.appendChild(listItemText);
-    document.getElementById('list').appendChild(listItem);
+        const buttonItem = document.createElement('button');
+        buttonItem.setAttribute('id', el.id)
+        const buttonItemText = document.createTextNode('Done');
+        buttonItem.appendChild(buttonItemText);
+
+        listItem.appendChild(buttonItem);
+
+        listElement.appendChild(listItem);
+    });
+}
+render();
+
+listElement.addEventListener('click', (event) => {
+    if(event.target.nodeName === 'BUTTON') {
+        const id = event.target.id
+        for(let i = 0; i < list.length; i++) {
+            if(list[i].id === id) {
+                list[i].done = !list[i].done;
+            }
+            updateLocalStorage();
+            render();
+        }
+    }
 })
+
+function addToList() {
+    const todoInputValue = todoInput.value;
+
+    list.push({
+        id: Math.random().toString(),
+        title: todoInputValue,
+        done: false
+    })
+
+    updateLocalStorage();
+    console.log(list);
+    render();
+
+    todoInput.value = '';
+}
+
+function updateLocalStorage() {
+    localStorage.setItem('list', JSON.stringify(list));
+}
